7ecac0afbabc31dbbdeccc1da51e5eff1379f385
sonia <wissal.jrad@gmail.com>
2016-05-04 18:25:20 +0000

    added validation and create option

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,22 @@ class ContactsController <ApplicationController
     end
     
     def create
+        @contact= Contact.new(contact_params)
+        if @contact.save
+            flash[:success]= 'Message Sent'
+            redirect_to new_contact_path
+        else
+            flash[:danger]= 'Error occured, message has not been sent'
+            redirect_to new_contact_path
+            
+        end
+        
+    end
+   
+    private
+    def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
+        
     end
     
 end

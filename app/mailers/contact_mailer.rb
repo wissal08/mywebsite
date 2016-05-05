44ab47f4615bd057bec18e84f7b531d0950fdf7a
sonia <wissal.jrad@gmail.com>
2016-05-05 10:42:17 +0000

    Added contact mailer view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,15 @@
+class ContactMailer < ActionMailer::Base
+    default to :'wissal.jrad@gmail.com'
+    
+    def contact_email(name, email, body)
+    @name=name
+    @email=email
+    @body=body
+    
+    mail(from:email, subject: 'Contact Form Message')
+    
+    
+    end
+    
+    
+end
